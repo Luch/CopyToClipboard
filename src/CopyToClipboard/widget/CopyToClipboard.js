26336e17816f250f84ce462095c5f2b6835b2151
Roeland Salij <roeland.salij@mendix.com>
2015-04-21 18:03:09 +0200

    Removed JQuery, clean- up, added test project

--- src/CopyToClipboard/widget/CopyToClipboard.js
+++ src/CopyToClipboard/widget/CopyToClipboard.js
@@ -18,13 +18,12 @@
 */
 // Required module list. Remove unnecessary modules, you can always get them back from the boilerplate.
 require({
-    packages: [{ name: 'jquery', location: '../../widgets/CopyToClipboard/lib', main: 'jquery-1.11.2.min' },
-			   { name: 'zeroclipboard', location: '../../widgets/CopyToClipboard/lib', main: 'ZeroClipboard.min' }]
+    packages: [{ name: 'zeroclipboard', location: '../../widgets/CopyToClipboard/lib', main: 'ZeroClipboard.min' }]
 }, [
     'dojo/_base/declare', 'mxui/widget/_WidgetBase', 'dijit/_TemplatedMixin',
-    'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-prop', 'dojo/dom-geometry', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/_base/array', 'dojo/_base/lang', 'dojo/text',
-    'jquery', 'zeroclipboard'
-], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domProp, domGeom, domClass, domStyle, domConstruct, dojoArray, lang, text, $, ZeroClipboard) {
+	'mxui/dom', 'dojo/dom', 'dojo/query', 'dojo/dom-class', 'dojo/dom-style', 'dojo/dom-construct', 'dojo/dom-attr', 'dojo/_base/lang', 'dojo/text', 'dojo/text!CopyToClipboard/widget/template/CopyToClipboard.html',
+    'zeroclipboard'
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, domQuery, domClass, domStyle, domConstruct, domAttr, lang, text, widgetTemplate, ZeroClipboard) {
     'use strict';
     
     // Declare widget's prototype.
@@ -39,79 +38,105 @@ require({
 		buttonstyle: "",
 
         // Internal variables. Non-primitives created in the prototype are shared between all widget instances.
-        _handle: null,
+        _handles: null,
         _contextObj: null,
-        _objProperty: null,
+		_zeroClipboardButton: null,
+		_targetElement: null,
+		_targetElementSelector : null,
 
         // dojo.declare.constructor is called to construct the widget instance. Implement to initialize non-primitive properties.
         constructor: function () {
-			this._objProperty = {};
+			this._handles = [];
         },
 
         // dijit._WidgetBase.postCreate is called after constructing the widget. Implement to do extra setup work.
         postCreate: function () {
-            console.log(this.id + '.postCreate');
-			var zcbwidget = $("#" + this.id);
-			// find element to copy from
-			var elementName = '.mx-name-' + this.elementnameString;
-			// get id of element
-			var elementId = $(elementName).attr('id');
-			var element = $(elementName);
-			element.addClass('CopyToClipboard_clip_area');
-			// attach copy button to widget
-			var zcbbuttonid = elementId + '_clip_button';
+            
+			var buttonImageNode = null;
+			this._targetElementSelector = '.mx-name-' + this.elementnameString;
+			this._targetElement = domQuery(this._targetElementSelector)[0];
+			
+			if(this._targetElement) {
+				domClass.add(this._targetElement,'CopyToClipboard_clip_area');
+			}
+			
 			// compose button html
-			var buttonhtml = '<button type="button" class="btn mx-button btn-default CopyToClipboard_clip_button ' + this.buttonclass + '" id=' + zcbbuttonid;
-			// tooltip
-			if (this.buttontooltip !== "") 
-				{buttonhtml = buttonhtml + ' title="' + this.buttontooltip + '"'};
-			// style
-			if (this.buttonstyle !== "") 
-				{buttonhtml = buttonhtml + ' style="' + this.buttonstyle + '"'};
-			buttonhtml = buttonhtml + ' >';
-			// buttonimage
-			if (this.buttonimage !== "") 
-				{buttonhtml = buttonhtml + '<img src='+ this.buttonimage + ' /> '};
-			buttonhtml = buttonhtml + this.buttoncaption + '</button>';
-			zcbwidget.append(buttonhtml);
-			zcbbuttonid = '#' + zcbbuttonid;
-
-			// initiate ZeroClipboard
-			ZeroClipboard.config({ swfPath: '/widgets/CopyToClipboard/lib/ZeroClipboard.swf' });
-			var clip = new ZeroClipboard($(zcbbuttonid));
- 
-			clip.on("ready", function(event) {
-				console.log("Flash movie loaded and ready.");
-				
-				clip.on("copy", function(event) {
-					//console.log("Copy");
-					event.clipboardData.setData('text/plain', $(elementName + " input").val());
-					event.clipboardData.setData('text/plain', $(elementName + " textarea").val());
-					event.clipboardData.setData('text/plain', $(elementName + " select :selected" ).text());
-				});
-
-				clip.on("aftercopy", function(event) {
-					console.log("Copied text to clipboard: " + event.data["text/plain"]);
-				});
+			this._zeroClipboardButton = domConstruct.create('button', {
+				'type' : 'button',
+				'class': 'btn mx-button btn-default CopyToClipboard_clip_button',
+				'id': domAttr.get(this._targetElement,'id') + '_clip_button',
+				'innerHTML': this.buttoncaption
 			});
+			domConstruct.place(this._zeroClipboardButton, this.domNode);
+			
+			// tooltip
+			if (this.buttontooltip !== "") {
+				domAttr.set(this._zeroClipboardButton, 'title', this.buttontooltip);
+			}
+			//class
+			if(this.buttonclass) {
+				domClass.add(this._zeroClipboardButton,this.buttonclass);
+			}
+			// style
+			if (this.buttonstyle !== "") {
+				domStyle.set(this._zeroClipboardButton, this.buttonstyle);
+			}
+			// buttonimage
+			if (this.buttonimage !== "") {
+				buttonImageNode = domConstruct.create('image', {
+					'src' : this.buttonimage
+				});
+				domConstruct.place(this.buttonImageNode, this._zeroClipboardButton);
+			}
+			this._initiateZeroClipboard();
+		},
+		
+		_initiateZeroClipboard : function () {
+			
+			ZeroClipboard.config({ swfPath: '/widgets/CopyToClipboard/lib/ZeroClipboard.swf' });
+			var clip = new ZeroClipboard(this._zeroClipboardButton);
+ 
+			clip.on("ready", lang.hitch(this, function(event) {
+				console.debug("Flash movie loaded and ready.");
+				
+				clip.on("copy",  lang.hitch(this, function(event) {
+					console.debug("Copy event");
+					
+					var inputNode, textareaNode = null, selectNode = null; 
+					
+					inputNode = domQuery(this._targetElementSelector + " input")[0];
+					if(inputNode) {
+						event.clipboardData.setData('text/plain', inputNode.value);
+						return;
+					}
+					
+					textareaNode = domQuery(this._targetElementSelector + " textarea")[0];
+					if(textareaNode) {
+						event.clipboardData.setData('text/plain', textareaNode.value);
+					}
+					
+					selectNode = domQuery(this._targetElementSelector + " select" )[0];
+					if(selectNode) {
+						event.clipboardData.setData('text/plain', selectNode.options[selectNode.selectedIndex].text);
+					}
+				}));
 
-			clip.on("error", function(event) {
+				clip.on("aftercopy", lang.hitch(this, function(event) {
+					console.log("Copied text to clipboard: " + event.data["text/plain"]);
+				}));
+			}));
+
+			clip.on("error", lang.hitch(this, function(event) {
 				console.log('CopyToClipboard error of type "' + event.name + '": ' + event.message);
 				ZeroClipboard.destroy();
-			});
-			
-            this._setupEvents();
+			}));
+
         },
 		
         // mxui.widget._WidgetBase.update is called when context is changed or initialized. Implement to re-render and / or fetch data.
         update: function (obj, callback) {
-            console.log(this.id + '.update');
-
             this._contextObj = obj;
-            this._resetSubscriptions();
-            this._updateRendering();
-
-            callback();
+			callback();
         },
 		
         // mxui.widget._WidgetBase.enable is called when the widget should enable editing. Implement to enable editing if widget is input widget.
@@ -132,36 +157,8 @@ require({
         // mxui.widget._WidgetBase.uninitialize is called when the widget is destroyed. Implement to do special tear-down work.
         uninitialize: function () {
             // Clean up listeners, helper objects, etc. There is no need to remove listeners added with this.connect / this.subscribe / this.own.
-        },
-
-        _setupEvents: function () {
-			console.log(this.id + '._setupEvents');
-        },
-
-        _updateRendering: function () {
-            
-        },
-
-        _resetSubscriptions: function () {
-            // Release handle on previous object, if any.
-            if (this._handle) {
-                this.unsubscribe(this._handle);
-                this._handle = null;
-            }
-
-            if (this._contextObj) {
-                this._handle = this.subscribe({
-                    guid: this._contextObj.getGuid(),
-                    callback: this._updateRendering
-                });
-				
-				//subscribe to changes in object attribute by other widgets
-				this._handle = this.subscribe({
-					guid: this._contextObj.getGuid(),
-					callback : function(guid, attr, value) {
-					}
-				});
-            }
+			ZeroClipboard.destroy();
         }
+
     });
 });
